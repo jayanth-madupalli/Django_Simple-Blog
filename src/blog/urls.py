33c822f500d5293f26b3dd6679fd0e1118a58cd1
JayanthM <jayanth.madupalli@gmail.com>
2020-04-11 15:08:53 +0530

    pagination, user posts

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='Blog-Home'),
+    path('user/<str:username>', views.UserPostListView.as_view(), name='User-Posts'),
     path('post/<int:pk>-<title>/', views.PostDetailView.as_view(), name='Post-Detail'),
     path('post/update/<int:pk>/', views.PostUpdateView.as_view(), name='Post-Update'),
     path('post/delete/<int:pk>/', views.PostDeleteView.as_view(), name='Post-Delete'),
